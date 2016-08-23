4408e0b7fef0bc7c8e26f9a88a28d82bcfddde60
Lucas Pena <Lucian@Lucian.local>
2016-08-23 16:22:41 +0200

    added gulp deploy

---
+++ gulp/deploy.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Lucian on 8/23/16.
+ */
+'use strict';
+
+var gulp = require('gulp');
+
+var $ = require('gulp-load-plugins')();
+var gulpsync = require('gulp-sync')(gulp);
+
+module.exports = function(options) {
+    gulp.task('deploy', gulpsync.sync(['clean', 'build']), function () {
+        return gulp.src(options.dist + '/**/*')
+            .pipe($.ghPages());
+    });
+};
