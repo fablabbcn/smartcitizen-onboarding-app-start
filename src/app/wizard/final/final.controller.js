f6dfb2d589a0361f4988bfa73ae17b920f9f6603
Dorian Somers <doriansomers@gmail.com>
2018-11-02 13:01:06 +0100

    fix #15 #20 hack to remove :hover for touch devices

--- src/app/wizard/final/final.controller.js
+++ src/app/wizard/final/final.controller.js
@@ -1,7 +1,6 @@
 export function finalController($scope, $rootScope, scopePayload, AnimationService){
     $scope.$parent.payload = scopePayload;
     AnimationService.animate(scopePayload.template);
-
     $scope.$parent.segueControl ='ready';
     // Maybe we use it for the final slide
 }
